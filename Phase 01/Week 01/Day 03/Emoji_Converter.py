5bb4502d8f62af2c6dd8e166910e228ea6c311db
Umanda Thathsarani <umathathsarani2003@gmail.com>
2026-06-24 16:26:19 +0530

    Add emoji converter script

---
+++ Phase 01/Week 01/Day 03/Emoji_Converter.py
@@ -0,0 +1,14 @@
+message = input(">")
+
+words = message.split(' ')
+
+emojis = {
+  ":)" : "😃",
+  ":(" : "🙁"
+}
+
+output = ""
+
+for word in words:
+  output += emojis.get(word,word) + " "
+print(output)
